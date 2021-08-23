a636da0fddc750d780386aeebf77b07ddb0bb177
lhes23 <32542078+lhes23@users.noreply.github.com>
2021-08-23 22:04:06 +0800

    add the database

--- todoProject/todo/urls.py
+++ todoProject/todo/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.urls.conf import include
 from . import views
 
 app_name = "todo"
@@ -6,4 +7,6 @@ app_name = "todo"
 urlpatterns = [
     path('', views.index, name="index"),
     path('add/',views.add,name="add"),
+    path('<task_id>',views.task,name="task"),
+    path('delete_task/<task_id>',views.delete_task,name="delete_task")
 ]
--- todoProject/todo/views.py
+++ todoProject/todo/views.py
@@ -1,13 +1,11 @@
 from django.http.response import HttpResponseRedirect
 from django.urls import reverse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import AddTaskForm
-
-# Create your views here.
-tasks = ["far", "foo", "bar"]
+from .models import Task
 
 def index(request):
-    context = {"tasks":tasks}
+    context = {"tasks":Task.objects.all()}
     return render(request,"todo/index.html",context)
 
 def add(request):
@@ -15,7 +13,8 @@ def add(request):
         form = AddTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            new_task = Task(task=task)
+            new_task.save()
             return HttpResponseRedirect(reverse("todo:index"))
         else:
             return render(request,'todo/add.html',{"form":form})
@@ -23,3 +22,12 @@ def add(request):
         form = AddTaskForm()
         context = {"form":form}
         return render(request,'todo/add.html',context)
+
+def task(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    return render(request, "todo/task.html",{"task":task})
+
+def delete_task(request,task_id):
+    task = Task.objects.get(pk=task_id)
+    task.delete()
+    return redirect('todo:index')
